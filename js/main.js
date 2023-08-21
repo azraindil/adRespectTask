5e76f433a3c32c6e8dafd76b3ef5754584b0729e
Marta Kowalczyk <m.kowalczyk@boosthigh.com>
2023-08-21 22:13:00 +0200

    added photos, content and styling for sections, footer

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
   var macyInstance = Macy({
     container: '.macy-container',
-    trueOrder: false,
+    trueOrder: true,
     waitForImages: false,
     margin: {
       x: 43,
@@ -13,8 +13,26 @@ document.addEventListener("DOMContentLoaded", function () {
       400: 1
     }
   });
+
   macyInstance.runOnImageLoad(function () {
     macyInstance.recalculate(true);
   }, true);
 
+  var macyBtn = document.getElementById("btn-gallery-expand");
+  var macyBtnText = document.getElementById("btn-macy-text-placeholder");
+  var macyBtnArrow = document.getElementById("arrow-bottom");
+  macyBtnText.textContent = "Rozwiń";
+  var macyContainer = document.getElementsByClassName("macy-container");
+  var macyGradient = document.getElementsByClassName("gallery-gradient");
+  macyBtn.addEventListener('click', () => {
+    macyContainer[0].classList.toggle("vh-100");
+    macyGradient[0].classList.toggle("d-none");
+    if (macyBtnText.textContent === "Rozwiń") {
+      macyBtnText.textContent = "Zwiń";
+      macyBtnArrow.style.transform = "rotate(-180deg)";
+    } else {
+      macyBtnText.textContent = "Rozwiń";
+      macyBtnArrow.style.transform = "rotate(0deg)";
+    }
+  })
 });
